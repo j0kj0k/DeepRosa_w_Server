e77848fd376b567f9ebba132f28817240969face
Jake <jikjikperez@gmail.com>
2023-10-11 12:52:08 +0800

    Added sorting to the server Modularized the serverDprosa.py file

--- Server/serverConnection.py
+++ Server/serverConnection.py
@@ -1,37 +1,66 @@
 import socket
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 from Server.serverDprosa import serverDprosa
 
+MAX_THREADS = 10  # Maximum number of threads in the thread pool
+thread_pool = ThreadPoolExecutor(max_workers=MAX_THREADS)
+
+timegap_dict = {}
+cluster_dict = {}
+global_var_lock = threading.Lock()
 
 def perform_cluster(client_socket,directory):
     # Perform action 1 based on the received data
     print("Performing cluster with directory:", directory)
     #client_socket.send(directory.encode('utf-8'))
-
+    global timegap_dict
+    global cluster_dict
     sD = serverDprosa()
+
     sD.compilereadCSV(directory)
     sD.cluster_event(directory)
+    timegap_dict,cluster_dict = sD.timegap_cluster()
+
+    sD.print_data()
 
     print("Clustering Done..")
-
-    client_socket.send("Compiling Done".encode('utf-8'))
+    print("*****************************************")
+    print("*****************************************")
+    client_socket.send("DONE.".encode('utf-8'))
     clientID = client_socket
 
     
 
-def perform_action2(data):
+def perform_sort(client_socket,data):
+    global timegap_dict
+    global cluster_dict
     # Perform action 2 based on the received data
-    print("Performing action 2 with data:", data)
+    print("Performing sorting with list:", data)
+
+    sD = serverDprosa()
+    itemList = sD.convertData(data)
+    sortedList, timegap_dict, cluster_dict = sD.sort_shoppingList(itemList, timegap_dict, cluster_dict)
+    sortedItem = ', '.join(sortedList)
+    print(sortedItem)
+    print("*****************************************")
+    print("*****************************************")
+    client_socket.send(sortedItem.encode('utf-8'))
+
 
 
 # Define a mapping of descriptions to functions
 ACTION_FUNCTIONS = {
     "cluster": perform_cluster,
-    "action2": perform_action2
+    "sort": perform_sort
 }
 
 def handle_client(client_socket):
+
+    thread_number = threading.get_ident()
+    print(f"Thread {thread_number}: Handling client connection.")
+
     while True:
         # Receive data and description from the client
         received_data = client_socket.recv(1024).decode('utf-8')
@@ -47,10 +76,15 @@ def handle_client(client_socket):
             # Call the appropriate function based on the description
             action_function = ACTION_FUNCTIONS[description]
             action_function(client_socket,data)
+            
+            break
+            thread_pool.submit(action_function, client_socket, data)
+
             client_socket.close()
         else:
             print("Unknown action description:", description)
-        
+                
+
 
     # Close the client socket when the connection is closed
     client_socket.close()
@@ -58,6 +92,7 @@ def handle_client(client_socket):
 def server():
     SERVER_ADDRESS = ('localhost', 8080)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
 
     try:
         server_socket.bind(SERVER_ADDRESS)
@@ -68,14 +103,22 @@ def server():
         return
     
 
-
+    with thread_pool:
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print("Accepted connection from", client_address)
+            thread_pool.submit(handle_client, client_socket)
+'''
     while True:
         client_socket, client_address = server_socket.accept()
         print("Accepted connection from", client_address)
 
         client_handler_thread = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler_thread.start()
+'''
+
     
 def start_server():
+
     server_thread = threading.Thread(target=server)
     server_thread.start()
--- Server/serverDprosa.py
+++ Server/serverDprosa.py
@@ -7,12 +7,29 @@ import csv
 import time
 import os
 
-#from Models._dprosa import \
-#    initialize_timegap, add_timegap, check_timegap, normalize_timegaps,\
-#    dict_to_matrix, agglomerative_clustering, kmeans_clustering, sort_shopping_list
+from Models._dprosa import \
+    initialize_timegap, add_timegap, check_timegap, normalize_timegaps,\
+    dict_to_matrix, agglomerative_clustering, kmeans_clustering, sort_shopping_list
 
 
 class serverDprosa():
+    def __init__(self):
+
+        super().__init__()
+        # initialize class variables
+        self.default_timegap = 10000
+        self.item_list = []
+        self.total_shoppers = 0
+        self.timegap_dict = {}
+        self.threshold_dict = {}
+        self.cluster_dict = {}
+        self.timegap_matrix = np.array([])
+        self.default_n_clusters = 0
+        self.default_distance_threshold = 60
+        self.shopping_list = []
+
+        self.threshold_var = 60
+        self.nclusters_var = 0
 
     def compilereadCSV(self,directory):
         self.start_time = time.time()
@@ -63,71 +80,36 @@ class serverDprosa():
         print("--- %s seconds ---    || AFTER SORTING" % (time.time() - self.start_time))
 
             
-        self.timegap_dict, self.total_shoppers = self.init_timegap()
+        self.timegap_dict, self.threshold_dict = initialize_timegap(self.item_list, self.default_timegap)
         print("--- %s seconds ---    || AFTER INIT TIMEGAP" % (time.time() - self.start_time))
-        self.timegap_dict, self.total_shoppers = self.add_timegap(df) 
-        print("--- %s seconds ---    || AFTER ADD TIMEGAP" % (time.time() - self.start_time))
-        self.timegap_dict = dict(sorted(self.timegap_dict.items(), key=lambda x: x[1]))     #sort from lowest timegap
+        self.timegap_dict, self.total_shoppers = add_timegap(df, self.timegap_dict, self.threshold_dict,True)
 
+        print("--- %s seconds ---    || AFTER ADD TIMEGAP" % (time.time() - self.start_time))
+        self.timegap_dict = normalize_timegaps(self.timegap_dict)
 
         print("--- %s seconds ---    || AFTER TIMEGAP DICT" % (time.time() - self.start_time))
             
+           
 
-    def init_timegap(self):
-        timegap_dict = {}
-        threshold_dict = {}
-        total_shoppers = 0
-        for key1 in self.item_list:
-            for key2 in self.item_list:
-                if key1 != key2:
-                    pair = tuple(sorted((key1, key2)))
-                    if pair not in timegap_dict:
-                        timegap_dict[pair] = [100000]
-
-        sorted_timegap_dict = dict(sorted(timegap_dict.items(), key=lambda x: x[0]))
-
-        for pair in sorted_timegap_dict:
-            threshold_dict[pair] = 0
-        
-        self.threshold_dict = threshold_dict
-        return sorted_timegap_dict, total_shoppers
-
-
-        def print_data(self):
-            self.running_time = 0    
-            print(f"Total Items: {len(self.item_list)}\n")
-            print(f"Total Pairs: {len(self.timegap_dict)}\n")
-            print(f"Total Shoppers: {self.total_shoppers}\n\n")
-            #print(f"Running time: {self.running_time:.2f}s\n\n")
+    def print_data(self):
+        self.running_time = 0    
+        print(f"Total Items: {len(self.item_list)}")
+        print(f"Total Pairs: {len(self.timegap_dict)}")
+        print(f"Total Shoppers: {self.total_shoppers}")
+        print(f"Total Clusters: {self.n_clusters}")
+        #print(f"Running time: {self.running_time:.2f}s\n\n")
 
     def cluster_event(self,directory):
-        self.dict_to_matrix()
+        self.timegap_matrix = dict_to_matrix(self.item_list, self.timegap_dict)
 
-        self.agglomerative_clustering()
+        self.cluster_dict, self.n_clusters= agglomerative_clustering(self.item_list, self.timegap_matrix, self.threshold_var, self.nclusters_var)
 
         #self.cluster_dendrogram()
         self.export_as_csv(directory)
-        self.centroid_dict = self.calculate_centroid()
+        #self.centroid_dict = self.calculate_centroid()
         #print(self.centroid_dict)
         #self.adjust_clusters()
 
-
-
-    def agglomerative_clustering(self):
-        k = 120
-        agglomerative = AgglomerativeClustering(n_clusters=None, metric='precomputed', linkage='average', distance_threshold=50)
-        cluster_labels = agglomerative.fit_predict(self.timegap_matrix)
-
-        clustered_items = {}
-        for label in set(cluster_labels):
-            clustered_items[label] = []
-
-        for item, label in zip(self.item_list, cluster_labels):
-            clustered_items[label].append(item)
-
-        self.cluster_dict = clustered_items
-        # print(self.cluster_dict)
-        self.k = 120
 
 
     def calculate_centroid(self):
@@ -152,117 +134,9 @@ class serverDprosa():
 
         return centroid_dict  
 
-    def sort_shopping_list(self):
-        self.sorted_list = self.shopping_list
-        self.cluster_anchor = 0
-        self.sorted_list = sorted(
-            self.sorted_list,
-            key=lambda x: (
-                -1 if any(x in value for value in self.cluster_dict.get(self.cluster_anchor-1, [])) else 0,
-                next((key for key, value in self.cluster_dict.items() if x in value), 0)
-            )
-        )   
-        for i in range(len(self.sorted_list) - 1):
-            item_x = self.sorted_list[i]
-            item_y = self.sorted_list[i + 1]
-            cluster_x = next((key for key, value in self.cluster_dict.items() if item_x in value), None)
-            cluster_y = next((key for key, value in self.cluster_dict.items() if item_y in value), None)
-
-            if cluster_x is not None and cluster_x == cluster_y:
-                continue
-
-            min_timegap = float('inf')
-            min_timegap_item = None
-
-            for j in range(i + 1, len(self.sorted_list)):
-                next_item = self.sorted_list[j]
-
-                if (item_x, next_item) in self.timegap_dict:
-                    timegap = self.timegap_dict[(item_x, next_item)]
-
-                    if timegap < min_timegap:
-                        min_timegap = timegap
-                        min_timegap_item = next_item
-
-            if min_timegap_item is not None:
-                index_min_timegap_item = self.sorted_list.index(min_timegap_item)
-                self.sorted_list[i + 1], self.sorted_list[index_min_timegap_item] = self.sorted_list[index_min_timegap_item], self.sorted_list[i + 1]
-
-        self.shopping_list = self.sorted_list
-        self.print_shopping_list()
-
     def print_shopping_list(self):
         for i, item in enumerate(self.shopping_list):
             self.print('end', f"{i+1}. {item}\n")
-
-    def add_timegap(self, df):
-        timegap_dict = self.timegap_dict
-        total_shoppers = self.total_shoppers
-        temp_dict = {}
-        print_max = len(df) // 5
-
-        for index in range(len(df)):
-            item_x, value_x, status_x = df.iloc[index]
-            value_x_next = 0 if index == len(df) - 1 else df.iloc[index + 1, 1]
-
-            if value_x == 0:
-                total_shoppers += 1
-                temp_dict.clear()
-                temp_dict[item_x] = value_x
-            else:
-                temp_dict[item_x] = value_x
-
-            if value_x_next == 0 and len(temp_dict) != 1:
-                sorted_keys = sorted(temp_dict.keys())
-                num_items = len(sorted_keys)
-
-                for i in range(num_items - 1):
-                    key1, key2 = sorted_keys[i], sorted_keys[i + 1]
-                    pair = tuple(sorted((key1, key2)))
-                    diff = abs(temp_dict[key1] - temp_dict[key2])
-
-                    if pair in timegap_dict:
-                        timegap_values = timegap_dict[pair]
-                        if diff < timegap_values[-1] - 10 or diff > timegap_values[-1] + 10:
-                            if pair not in self.threshold_dict:
-                                self.threshold_dict[pair] = 1
-                            else:
-                                self.threshold_dict[pair] += 1
-
-                            if self.threshold_dict[pair] >= 3:
-                                timegap_values.append(sum(timegap_values[-3:]) / 3)
-                                self.threshold_dict[pair] = 0
-                        timegap_values.append(diff)
-                    else:
-                        timegap_dict[pair] = [diff]
-
-                temp_dict.clear()
-
-        for values in timegap_dict.values():
-            if len(values) > 1:
-                values.pop(0)
-
-        for key in timegap_dict:
-            timegap_dict[key] = sum(timegap_dict[key]) / len(timegap_dict[key])
-
-        sorted_timegap_dict = dict(sorted(timegap_dict.items(), key=lambda x: x[0]))
-        return sorted_timegap_dict, total_shoppers
-
-
-
-    def dict_to_matrix(self):
-        items = self.item_list
-        matrix = np.zeros((len(items), len(items)))
-
-        for i in range(len(items)):
-            for j in range(len(items)):
-                if i != j:
-                    key = (items[i], items[j])
-                    if key in self.timegap_dict:
-                        matrix[i][j] = self.timegap_dict[key]
-                        matrix[j][i] = self.timegap_dict[key]
-
-        self.timegap_matrix = matrix
 
     def export_as_csv(self,directory):
 
@@ -290,3 +164,17 @@ class serverDprosa():
         print("--- %s seconds ---    || DONE..." % (time.time() - self.start_time))
         
         return csv_string
+    
+    def timegap_cluster(self):
+        return self.timegap_dict, self.cluster_dict
+    
+    '''------------------------------------------------------------ SORTING ------------------------------------------------------------'''
+    def sort_shoppingList(self,shopping_list, timegap_dict, cluster_dict):
+        shopping_list = sort_shopping_list(None, shopping_list, timegap_dict, cluster_dict)
+        return shopping_list, timegap_dict, cluster_dict
+    
+    def convertData(self,string):
+        itemlist = string.split(',')
+        # Remove empty elements from the list
+        itemlist = [item.strip() for item in itemlist if item.strip()]
+        return itemlist
